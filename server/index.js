57c0d400043b2abf0f70121a5592a4399016a75d
AmanJ10 <joharapurkaraman95@gmail.com>
2024-08-02 09:11:59 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -71,7 +71,7 @@ app.post("/login", async (req, res) => {
           res
             .cookie("token", token, {
               httpOnly: true,
-              secure: false,
+              secure: true,
               sameSite: "lax",
             })
             .json(userDoc);
